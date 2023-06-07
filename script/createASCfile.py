5d682526e79f434db0da659cd0402555d23c4f61
Ramune6110 <Arduino6110@gmail.com>
2023-06-08 01:01:54 +0900

    [modify]createASCfile

--- script/createASCfile.py
+++ script/createASCfile.py
@@ -26,6 +26,20 @@ for i in range(100):  # 100メッセージを生成
             bitrate_switch=self.bitrate_switch,
             error_state_indicator=self.error_state_indicator,
         )
+        
+    timestamp: メッセージが受信された時間を秒単位で表す浮動小数点数です。この時間は、プログラムの開始時点を基準としたもので、時計の時間とは異なります。
+    arbitration_id: メッセージの識別子 (ID) で、メッセージの優先度を決定します。値は通常、整数です。
+    is_extended_id: メッセージが拡張ID (29ビット) を使用している場合にTrue、標準ID (11ビット) を使用している場合にFalseを返します。
+    is_remote_frame: メッセージがリモートフレームである場合にTrueを返します。リモートフレームは、特定のデータを要求するためのフレームです。
+    is_error_frame: メッセージがエラーフレームである場合にTrueを返します。エラーフレームは、通信上の問題を示すためのフレームです。
+    channel: メッセージが属するチャネルを示す文字列または整数です。通常、物理インターフェースまたは仮想チャネルを示します。
+    dlc: メッセージのデータ長（Data Length Code）です。これはメッセージのペイロード部分に含まれるバイト数を示します。
+    data: メッセージのデータペイロードです。これは一般にバイトのリストまたはバイト列です。
+    is_fd: メッセージがCAN FDフレームである場合にTrueを返します。CAN FDは、データレートとデータ長が拡張されたCANのバージョンです。
+    is_rx: メッセージが受信されたものである場合にTrue、送信されたものである場合にFalseを返します。
+    bitrate_switch: CAN FDメッセージがビットレートスイッチを使用している場合にTrueを返します。これはデータ部分が高速ビットレートで送信されていることを示します。
+    error_state_indicator: CAN FDメッセージのエラー状態インジケータが有効である場合にTrueを返します。これは送信側のエラー状態を示します。
+
     """
     msg = Message(timestamp=time.time() - start_time,  # 現在の時間から開始時間を引いたものをタイムスタンプとする
                   arbitration_id=randint(0, 0x7FF),  # 0から0x7FFの間でランダムなIDを選択
